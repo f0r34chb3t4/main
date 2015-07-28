ce44880bd2bfe81f3f18245f79749f3adb781458
f0r34chb3t4 <f0r34chb3t4@gmail.com>
2015-07-28 14:01:12 -0300

    Create eval.php

---
+++ eval.php
@@ -0,0 +1,18 @@
+<?php
+
+/* commando a ser executado */
+$cmd = 'var_dump(php_uname());';
+
+
+/* encode de string de comando */
+$str = sprintf('eval(base64_decode("%s"));', base64_encode($cmd));
+
+
+/* imprimir comando encoded */
+echo $str , PHP_EOL;
+
+
+/* executar comando */
+eval($str);
+
+?>
